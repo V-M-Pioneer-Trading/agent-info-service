d3be0af30a64bdbcc36f24a0385b4e17c00a5e6b
Max <maxradomskyy@gmail.com>
2024-06-13 21:14:58 +0200

    ADD: initial database module

---
+++ src/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetDatabase() *sql.DB {
+
+	log.Default().Printf("Establishing connection to MySql DB...")
+
+	// Configure the database connection (always check errors)
+	db, err := sql.Open("mysql", "user:pass@(127.0.0.1:3306)/vnm-agent-db?parseTime=true")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Default().Printf("Connection to DB is established.")
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	log.Default().Printf("Database ready.")
+
+	return db
+}
