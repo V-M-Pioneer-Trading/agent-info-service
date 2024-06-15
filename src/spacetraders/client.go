a0a4defdf044ea5e4adfabf1b35565590f98fdbd
Max <maxradomskyy@gmail.com>
2024-06-15 23:05:01 +0200

    change: use api response models in actual api calls

--- src/spacetraders/client.go
+++ src/spacetraders/client.go
@@ -2,8 +2,11 @@ package spacetraders
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"reflect"
+	"vnm/agent-info-service/spacetraders/schema"
 )
 
 const BASE_URL = "https://api.spacetraders.io/v2"
@@ -12,30 +15,38 @@ const BASE_URL = "https://api.spacetraders.io/v2"
 const BEARER_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoiVEVTVC1WTk0tMTQtMDYiLCJ2ZXJzaW9uIjoidjIuMi4wIiwicmVzZXRfZGF0ZSI6IjIwMjQtMDYtMDkiLCJpYXQiOjE3MTgzNjk1OTQsInN1YiI6ImFnZW50LXRva2VuIn0.qYclJL6OrzfoMaM9jMxPqL_q2L-uD_bRNthcwITZANAP5Uod30PiAHj1oPlcy4sY61dIeBvNG-xShwVYaVnBTd1DaMM4hG4ugnBX6JeMLVuR8vybrP2eNLMBs9iiFRcGRumD-PpFH0b7ggHB17PiJEyrwOxkb_rr49-LwAE_nHHNKH8Z9VcqiJ0Rxyz_LMTtdIGlQxQ1qdM4y4J7_fWDpqaa3i-Z8pIJddtgp0ioGf6_frOefHu7Ddps3LEzl1cC5eQp68aQs06jnJIXc4DZp60TAjW08xEBrjxCR5a5No_iwKKezZ6-91DoC3A4dmjcc2zFYCXVrqfvaPCaIkBecQ"
 
 func GetMyAgent() {
-	data, err := makeAuthenticatedGetRequest("/my/agent")
+
+	var agent schema.GetMyAgentResponse
+
+	data, err := makeAuthenticatedGetRequest("/my/agent", agent)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Parsed response: %v\n", data)
+	agentResponse := data.(schema.GetMyAgentResponse)
+	fmt.Printf("Agent response:\n %+v\n", agentResponse)
 }
 
 func GetMyShips() {
-	data, err := makeAuthenticatedGetRequest("/my/ships")
+	var ships schema.GetMyShipsResponse
+	data, err := makeAuthenticatedGetRequest("/my/ships", ships)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Parsed response: %v\n", data)
+	requestResult := data.(schema.GetMyShipsResponse)
+	fmt.Printf("Ships response: %v\n", requestResult)
 }
 
 func GetMyContracts() {
-	data, err := makeAuthenticatedGetRequest("/my/contracts")
+	var contracts schema.GetMyContractsResponse
+	data, err := makeAuthenticatedGetRequest("/my/contracts", contracts)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Parsed response: %v\n", data)
+	requestResult := data.(schema.GetMyContractsResponse)
+	fmt.Printf("Contracts response: %v\n", requestResult)
 }
 
-func makeAuthenticatedGetRequest(endpoint string) (interface{}, error) {
+func makeAuthenticatedGetRequest(endpoint string, resultType interface{}) (interface{}, error) {
 	req, err := http.NewRequest("GET", BASE_URL+endpoint, nil)
 	if err != nil {
 		return nil, err
@@ -50,11 +61,19 @@ func makeAuthenticatedGetRequest(endpoint string) (interface{}, error) {
 	}
 	defer resp.Body.Close()
 
-	var result interface{}
-	err = json.NewDecoder(resp.Body).Decode(&result)
+	// Check if the response status code is OK
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("failed to get a successful response from the server")
+	}
+
+	// Create a new instance of the resultType using reflection
+	resultValue := reflect.New(reflect.TypeOf(resultType)).Interface()
+
+	err = json.NewDecoder(resp.Body).Decode(resultValue)
 	if err != nil {
 		return nil, err
 	}
 
-	return result, nil
+	// Return the dereferenced value
+	return reflect.ValueOf(resultValue).Elem().Interface(), nil
 }
